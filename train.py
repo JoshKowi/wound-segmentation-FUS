763994f154255363e9856f9d7ecef0a89e5e6f61
JoshKowi <josh.kowalzik@gmail.com>
2025-02-10 15:06:01 +0100

    Make train.py executable with arguments

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import sys
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.models import load_model
@@ -17,53 +18,62 @@ from utils.io.data import DataGen, save_results, save_history, load_data
 #os.system('export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}')
 #os.system('export PATH=/usr/local/cuda-10.0/bin:/usr/local/cuda-10.0/NsightCompute-1.0${PATH:+:${PATH}}')
 
-# Varibales and data generator
-input_dim_x=512
-input_dim_y=512
-n_filters = 32
-dataset = 'fuseg_augmented'
-data_gen = DataGen('./data/' + dataset + '/', x=input_dim_x, y=input_dim_y)
+def train_model(dataset, batch_size=8, epochs=100, patience=10, input_dims=(512,512)):
+    # Varibales and data generator
+    input_dim_x, input_dim_y = input_dims
+    n_filters = 32
+    dataset = dataset
+    data_gen = DataGen('./data/' + dataset + '/', x=input_dim_x, y=input_dim_y)
 
-######### Get the deep learning models #########
+    ######### Get the deep learning models #########
 
-######### Unet ##########
-# unet2d = Unet2D(n_filters=n_filters, input_dim_x=None, input_dim_y=None, num_channels=3)
-# model, model_name = unet2d.get_unet_model_yuanqing()
+    ######### Unet ##########
+    # unet2d = Unet2D(n_filters=n_filters, input_dim_x=None, input_dim_y=None, num_channels=3)
+    # model, model_name = unet2d.get_unet_model_yuanqing()
 
-######### MobilenetV2 ##########
-model = Deeplabv3(input_shape=(input_dim_x, input_dim_y, 3), classes=1)
-model_name = 'MobilenetV2'
-#with CustomObjectScope({'relu6': relu6,'DepthwiseConv2D': DepthwiseConv2D, 'BilinearUpsampling': BilinearUpsampling}):
-#    model = load_model('training_history/2019-12-19 01%3A53%3A15.480800.hdf5'
-#                       , custom_objects={'dice_coef': dice_coef, 'precision':precision, 'recall':recall})
+    ######### MobilenetV2 ##########
+    model = Deeplabv3(input_shape=(input_dim_x, input_dim_y, 3), classes=1)
+    model_name = 'MobilenetV2'
+    #with CustomObjectScope({'relu6': relu6,'DepthwiseConv2D': DepthwiseConv2D, 'BilinearUpsampling': BilinearUpsampling}):
+    #    model = load_model('training_history/2019-12-19 01%3A53%3A15.480800.hdf5'
+    #                       , custom_objects={'dice_coef': dice_coef, 'precision':precision, 'recall':recall})
 
-######### Vgg16 ##########
-# model, model_name = FCN_Vgg16_16s(input_shape=(input_dim_x, input_dim_y, 3))
+    ######### Vgg16 ##########
+    # model, model_name = FCN_Vgg16_16s(input_shape=(input_dim_x, input_dim_y, 3))
 
-######### SegNet ##########
-# segnet = SegNet(n_filters, input_dim_x, input_dim_y, num_channels=3)
-# model, model_name = segnet.get_SegNet()
+    ######### SegNet ##########
+    # segnet = SegNet(n_filters, input_dim_x, input_dim_y, num_channels=3)
+    # model, model_name = segnet.get_SegNet()
 
-# plot_model(model, to_file=model_name+'.png')
+    # plot_model(model, to_file=model_name+'.png')
 
-# training
-batch_size = 8
-epochs = 2000
-learning_rate = 1e-4
-loss = 'binary_crossentropy'
+    # training
+    batch_size = batch_size
+    epochs = epochs
+    learning_rate = 1e-4
+    loss = 'binary_crossentropy'
 
-es = EarlyStopping(monitor='val_dice_coef', patience=30, mode='max', restore_best_weights=True)
-#training_history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs
-#                             , validation_split=0.2, verbose=1, callbacks=[])
+    es = EarlyStopping(monitor='val_dice_coef', patience=patience, mode='max', restore_best_weights=True)
+    #training_history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs
+    #                             , validation_split=0.2, verbose=1, callbacks=[])
 
-model.summary()
-model.compile(optimizer=Adam(learning_rate=learning_rate), loss=loss, metrics=[IoU, dice_coef, precision, recall])
-training_history = model.fit(data_gen.generate_data(batch_size=batch_size, train=True),
-                                       steps_per_epoch=int(data_gen.get_num_data_points(train=True) / batch_size),
-                                       callbacks=[es],
-                                       validation_data=data_gen.generate_data(batch_size=batch_size, val=True),
-                                       validation_steps=int(data_gen.get_num_data_points(val=True) / batch_size),
-                                       epochs=epochs)
-### save the model weight file and its training history
-save_history(model, model_name, training_history, dataset, n_filters, epochs, learning_rate, loss, color_space='RGB',
-             path='./training_history/')
+    model.summary()
+    model.compile(optimizer=Adam(learning_rate=learning_rate), loss=loss, metrics=[IoU, dice_coef, precision, recall])
+    training_history = model.fit(data_gen.generate_data(batch_size=batch_size, train=True),
+                                           steps_per_epoch=int(data_gen.get_num_data_points(train=True) / batch_size),
+                                           callbacks=[es],
+                                           validation_data=data_gen.generate_data(batch_size=batch_size, val=True),
+                                           validation_steps=int(data_gen.get_num_data_points(val=True) / batch_size),
+                                           epochs=epochs)
+    ### save the model weight file and its training history
+    save_history(model, model_name, training_history, dataset, n_filters, epochs, learning_rate, loss, color_space='RGB',
+                 path='./training_history/')
+
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if len(args)<1:
+        train_model('fuseg_augmented')
+    else:
+        train_model(args[0], int(args[1]), int(args[2]), int(args[3]))
