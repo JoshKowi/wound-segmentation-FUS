f7782dcbff77a1b2cf061a7ee0bdb5b0a09dc016
JoshKowi <josh.kowalzik@gmail.com>
2025-02-18 15:07:36 +0100

    Create Callback Method saving val_predictions after each epoch

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ from models.SegNet import SegNet
 
 from utils.learning.metrics import dice_coef, precision, recall, IoU
 from utils.learning.losses import dice_coef_loss
+from utils.learning.callbacks import PrintValidations
 from utils.io.data import DataGen, save_results, save_history, load_data
 
 
@@ -54,14 +55,15 @@ def train_model(dataset, batch_size=8, epochs=100, patience=10, input_dims=(512,
     loss = 'binary_crossentropy'
 
     es = EarlyStopping(monitor='val_dice_coef', patience=patience, mode='max', restore_best_weights=True)
+    print_vals = PrintValidations(data_gen)
     #training_history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs
     #                             , validation_split=0.2, verbose=1, callbacks=[])
 
     model.summary()
-    model.compile(optimizer=Adam(learning_rate=learning_rate), loss=loss, metrics=[IoU, dice_coef, precision, recall])
+    model.compile(optimizer=Adam(learning_rate=learning_rate), loss=loss, metrics=[IoU, dice_coef, precision, recall], run_eagerly=False, steps_per_execution=4)
     training_history = model.fit(data_gen.generate_data(batch_size=batch_size, train=True),
                                            steps_per_epoch=int(data_gen.get_num_data_points(train=True) / batch_size),
-                                           callbacks=[es],
+                                           callbacks=[es, print_vals],
                                            validation_data=data_gen.generate_data(batch_size=batch_size, val=True),
                                            validation_steps=int(data_gen.get_num_data_points(val=True) / batch_size),
                                            epochs=epochs)
--- utils/learning/callbacks.py
+++ utils/learning/callbacks.py
@@ -5,9 +5,14 @@
 # Custom callbacks
 #
 # ------------------------------------------------------------ #
-
+import keras.src.callbacks
 import numpy as np
-from keras.callbacks import LearningRateScheduler
+import os
+import datetime
+import tensorflow as tf
+from tensorflow.keras.callbacks import LearningRateScheduler, Callback
+from utils.io.data import save_results
+
 
 # reduce learning rate on each epoch
 def learningRateSchedule(initialLr=1e-4, decayFactor=0.99, stepSize=1):
@@ -16,3 +21,21 @@ def learningRateSchedule(initialLr=1e-4, decayFactor=0.99, stepSize=1):
         print("Learning rate : ", lr)
         return lr
     return LearningRateScheduler(schedule)
+
+class PrintValidations(keras.callbacks.Callback):
+    '''Callback to print the models predictions on the same 3 validation-images
+    for each epoch.'''
+
+    def __init__(self, dataGenerator):
+        super().__init__()
+        self.batch_size = 3
+        self.val_images , _ = next(dataGenerator.generate_data(batch_size=self.batch_size, val=True))
+        self.epoch = 0
+        self.preds_directory = f"training_history/val_predictions-{datetime.datetime.now().strftime('%Y-%m-%d--%H-%M')}/"
+        os.makedirs(self.preds_directory, exist_ok=True)
+
+    def on_epoch_end(self, epoch, logs=None):
+        prediction_names = [f"ep-{self.epoch}-img-{i}.png" for i in range(self.batch_size)] # create filename list
+        self.epoch +=1
+        prediction = self.model.predict(self.val_images, verbose=1)
+        save_results(prediction, 'rgb', self.preds_directory, prediction_names)
